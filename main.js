bc82940247a27616316cbc5a918e597324bdcbcd
Vanderson Nascimento <vando.nsc@gmail.com>
2023-04-26 20:06:35 -0300

    Adicionada mais uma constante e função - Adicionada  função de escrever no placeholder e o valor ir para o html
    - Solucionado problema de repetição de valores

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
  const form = document.getElementById("form-itens")
  const itensInput = document.getElementById("receber-item")
+ const ulItens = document.getElementById("lista-de-itens")
+
 
  form.addEventListener("submit", function(evento){
     evento.preventDefault() //impedir qu algo aconteça
     salvarItem()
+    mostrarItem()
  })
 
  function salvarItem(){
@@ -22,4 +25,20 @@
     })
     }
     console.log(listaDeItens)
+ }
+
+ function mostrarItem(){
+    ulItens.innerHTML = ''
+    listaDeItens.forEach((elemento,index) => {
+        ulItens.innerHTML += ` 
+     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+     </li>`
+    })
  }
