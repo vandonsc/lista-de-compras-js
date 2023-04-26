2fb11dc499ca08a949871cb9480a603801b06df3
Vanderson Nascimento <vando.nsc@gmail.com>
2023-04-26 19:30:39 -0300

    Implementado funções de salvar valores e condicões

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
  let listaDeItens = []
 
- const form = Document.getElementById("form-itens")
- const itensInput = Document.getElementById("receber-item")
- 
+ const form = document.getElementById("form-itens")
+ const itensInput = document.getElementById("receber-item")
+
+ form.addEventListener("submit", function(evento){
+    evento.preventDefault() //impedir qu algo aconteça
+    salvarItem()
+ })
+
+ function salvarItem(){
+    const comprasItem = itensInput.value 
+    const checarDuplicado = listaDeItens.some ((elemento)=> elemento.valor.toUpperCase() === comprasItem.toUpperCase())
+
+    if(checarDuplicado == true){
+        alert("Item ja exsite")
+    }else{
+
+
+    listaDeItens.push( {
+        valor: comprasItem
+    })
+    }
+    console.log(listaDeItens)
+ }
