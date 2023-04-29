61e2b912062105c0e019d5d962abb2e390b60902
Vanderson Nascimento <vando.nsc@gmail.com>
2023-04-28 22:32:34 -0300

    Feito edição da lista e informacões fixas na tela - Feito a parte de edição e remoção da lista - Usado stringify para passar as informações em JSON no local storage - Implementado métodos setItem e getItem para enviar e recuperar dados.

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
  let listaDeItens = []
+ let itemAEditar
 
  const form = document.getElementById("form-itens")
  const itensInput = document.getElementById("receber-item")
  const ulItens = document.getElementById("lista-de-itens")
  const ulItensComprados = document.getElementById("itens-comprados")
+ const listaRecuperada = localStorage.getItem('listaDeItens')//getItem pega informações
+ function atualizaLocalStorage(){
+    localStorage.setItem('listaDeItens' , JSON.stringify(listaDeItens)) //.stringify transforma elementos em dados do tipo string
+ }
+
+ //o que retorna false em uma condição em js: (valores omitidos, 0, null, NaN. undefined, "", false) << retornam false
+
+if(listaRecuperada){
+    listaDeItens = JSON.parse(listaRecuperada) //.parse transforma elementos em dados do tipo javascript
+    mostrarItem()
+}else{
+    listaDeItens = []
+}
 
  form.addEventListener("submit", function(evento){
     evento.preventDefault() //impedir que algo aconteça
@@ -56,6 +70,7 @@
             <input type="text" class="is-size-5" value="${elemento.valor}"></input>
         </div>
         <div>
+            <button onclick= "salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
      </li>`
@@ -70,5 +85,41 @@
          listaDeItens[valorDoELemento].checar = evento.target.checked
          mostrarItem()
         })
+    
     })
+
+    const deletarObjetos =  document.querySelectorAll(".deletar")
+
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+         const valorDoELemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+         listaDeItens.splice(valorDoELemento, 1)
+         mostrarItem()
+         
+        })
+    
+    })
+
+    const editarItens = document.querySelectorAll(".editar")
+
+   editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+         itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+         mostrarItem()
+         
+        })
+    
+    })
+
+    atualizaLocalStorage()
+ }
+
+ function salvarEdicao(){
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+   // console.log(itemEditado.value)
+    listaDeItens[itemAEditar].valor = itemEditado.value
+    console.log(listaDeItens)
+    itemAEditar = -1
+    mostrarItem()
+
  }
